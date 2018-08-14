e32fb8edb4ba61dcfb3cff7d6479f2ad101a6280
Aaron C Robinson <>
2018-08-14 01:16:52 -0400

    Heyo
    -Adds rule for no base building
    -Adds new scenerio to use new rule

---
+++ Source/NomadsLand/Rule_DisallowAllBuilding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace NomadsLand
+{
+    public class Rule_DisallowAllBuilding : ScenPart_Rule
+    {
+        protected override void ApplyRule()
+        {
+            Current.Game.GetComponent<NomadsLand_RulesExt>().DisallowBuildings = true;
+        }
+    }
+}
---
+++ Source/NomadsLand/RulesExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace NomadsLand
+{
+    public class NomadsLand_RulesExt : GameComponent
+    {
+        private bool disallowBuildings = false;
+        public bool DisallowBuildings { get; set; }
+
+        public NomadsLand_RulesExt() { }
+
+        public NomadsLand_RulesExt(Game game) { }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look<bool>(ref this.disallowBuildings, "disallowAllBuilding", false);
+        }
+    }
+}
